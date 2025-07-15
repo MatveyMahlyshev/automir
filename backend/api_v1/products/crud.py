1437fe8ee2b742c1227ef0032fd436aa20a13096
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-07-15 16:24:54 +0300

    create car function, need access to frontend

--- backend/api_v1/products/crud.py
+++ backend/api_v1/products/crud.py
@@ -1,46 +1,71 @@
-from fastapi import UploadFile
+from fastapi import UploadFile, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from fastapi.exceptions import HTTPException, ValidationException
 
 from .schemas import ProductCreateCar
 from core.models import Car, Product, ProductImage
-from services.s3 import upload_file_to_s3
+from services.s3 import upload_file_to_s3, delete_file_from_s3
 
 
 async def create_auto(
     product: ProductCreateCar, images: list[UploadFile], session: AsyncSession
 ):
-    car = Car(
-        title=product.title,
-        engine=product.engine,
-        fuel_type=product.fuel_type,
-        year=product.year,
-        transmission=product.transmission,
-        drivetrain=product.drivetrain,
-        body_type=product.body_type,
-        color=product.color,
-        mileage=product.mileage,
-        owners=product.owners,
-        steering_wheel=product.steering_wheel,
-        generation=product.generation,
-        trim_level=product.trim_level,
-        description=product.description,
-    )
-    session.add(car)
-    await session.flush()
+    uploaded_images_keys = []
+    try:
+        car = Car(
+            title=product.title,
+            engine=product.engine,
+            fuel_type=product.fuel_type,
+            year=product.year,
+            transmission=product.transmission,
+            drivetrain=product.drivetrain,
+            body_type=product.body_type,
+            color=product.color,
+            mileage=product.mileage,
+            owners=product.owners,
+            steering_wheel=product.steering_wheel,
+            generation=product.generation,
+            trim_level=product.trim_level,
+            description=product.description,
+        )
+        session.add(car)
+        await session.flush()
 
-    car_product = Product(price=product.price, type="car", car_id=car.id)
-    session.add(car_product)
-    await session.flush()
+        car_product = Product(price=product.price, type="car", car_id=car.id)
+        session.add(car_product)
+        await session.flush()
 
-    for img in images:
-        content = await img.read()
-        image_url = upload_file_to_s3(content, car_product.id, img.content_type)
-        image = ProductImage(product_id=car_product.id, image=image_url)
-        session.add(image)
+        urls = []
 
-    await session.commit()
-    return {
-        "car": car,
-        "product": car_product,
-        "images": "http://localhost:9000/media/",
-    }
+        for img in images:
+
+            try:
+                content = await img.read()
+
+                s3_image = await upload_file_to_s3(content, car_product.id, img.content_type)
+                uploaded_images_keys.append(s3_image["unique_name"])
+                urls.append(s3_image["url"])
+
+                db_image = ProductImage(product_id=car_product.id, image=s3_image["url"])
+                session.add(db_image)
+            finally:
+                await img.close()
+
+        await session.commit()
+        return {
+            "car": car,
+            "product": car_product,
+            "images": urls,
+        }
+    except Exception as e:
+        await session.rollback()
+        for key in uploaded_images_keys:
+            await delete_file_from_s3(key)
+
+        if isinstance(e, IntegrityError):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+            )
--- backend/services/s3.py
+++ backend/services/s3.py
@@ -1,19 +1,47 @@
-import boto3
+import aioboto3
 import uuid
 
-s3 = boto3.client(
-    "s3",
-    endpoint_url="http://localhost:9000",
-    aws_access_key_id="admin",
-    aws_secret_access_key="password123",
-)
-
 BUCKET_NAME = "media"
+ENDPOINT_URL = "http://localhost:9000"
+AWS_ACCESS_KEY_ID = "admin"
+AWS_SECRET_ACCESS_KEY = "password123"
+
+session = aioboto3.Session()
 
 
-def upload_file_to_s3(file_content: bytes, file_id: int, content_type: str) -> str:
+async def upload_file_to_s3(file_content: bytes, file_id: int, content_type: str):
     unique_name = f"{uuid.uuid4()}_{file_id}"
-    s3.put_object(
-        Bucket=BUCKET_NAME, Key=unique_name, Body=file_content, ContentType=content_type
-    )
-    return f"http://localhost:9000/{BUCKET_NAME}/{unique_name}"
+
+    async with session.client(
+        "s3",
+        endpoint_url=ENDPOINT_URL,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    ) as s3:
+        await s3.put_object(
+            Bucket=BUCKET_NAME,
+            Key=unique_name,
+            Body=file_content,
+            ContentType=content_type,
+            ACL="public-read",
+        )
+
+    return {
+        "url": f"{ENDPOINT_URL}/{BUCKET_NAME}/{unique_name}",
+        "unique_name": unique_name,
+    }
+
+
+async def delete_file_from_s3(key: str) -> bool:
+    try:
+        async with session.client(
+            "s3",
+            endpoint_url=ENDPOINT_URL,
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        ) as s3:
+            await s3.delete_object(Bucket=BUCKET_NAME, Key=key)
+        return True
+    except Exception as e:
+        print(e)
+        return False
