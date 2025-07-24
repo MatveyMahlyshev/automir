e61768c2e4184b54040b441adb92b894496caeea
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-07-24 16:24:43 +0300

    add origin

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app.include_router(router=api_router, prefix=settings.api_v1_prefix)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://192.168.0.7:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
